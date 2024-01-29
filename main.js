03d9d4043372e1a4ce791195843c14eba74934f3
“Mona <monaelizabet@gmail.com>
2024-01-29 15:38:55 +0100

    Co-authored-by: RozlinA <RozlinA@users.noreply.github.com> Co-authored-by: MickeCardaba <freesideMike@users.noreply.github.com>

--- main.js
+++ main.js
@@ -6,3 +6,43 @@ const bodyParser = require ("body-parser")
 
 app.use(cors())
 app.use(bodyParser.json())
+
+function getNextId(){
+    let m = Math.max(...users.map(user => user.id))
+    return m + 1
+}
+
+
+let users = [{
+   userName: "user1",
+   password: "1998",
+   id: 1
+}]
+
+let messages= [{
+    userName: "user1",
+    message: "hej hopp trallalla",
+    time: "18.30.45"
+ }
+]
+app.get('/api/chat',(req,res)=>{
+    let result = users.map(user=>({
+        userName: user.userName,
+        password: user.password,
+    }))
+     res.json(result)
+});
+
+app.post('/api/chat',(req,res)=>{
+  const user = {
+      userName : req.body.userName,
+      password: req.body.password,
+      id:getNextId()
+  }
+  users.push(user)
+  res.status(201).send('Created')
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening2 on port ${port}`)
+})
