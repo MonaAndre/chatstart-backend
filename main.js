633cdc2070995c3c03f9acfc293ce1b16e7d968a
Rözlin Akkurt <rozlin.akkurt@hotmail.com>
2024-02-19 16:01:11 +0100

    server validation
    Co-authored-by: puuri <puuri@users.noreply.github.com>
    Co-authored-by: MickeCardaba <freesideMike@users.noreply.github.com>
    Co-authored-by: Mona <MonaAndre@users.noreply.github.com>

--- main.js
+++ main.js
@@ -6,13 +6,16 @@ const {sequelize, User} = require ('./models')
 const migrationhelper = require('./migrationhelper')
 const session = require('express-session');
 const userController = require('./controllers/userController.js')
+const {check, validationResult} = require('express-validator')
+const {requireAuth} = require('./middlewares/requireAuth.js')
+const {validateCreateUser} = require('./validators/userValidators.js')
 
 
 app.use(express.json())
 
 
 app.use(cors({
-    origin:"http://127.0.0.1:5500",
+    origin:"http://127.0.0.1:5501",
     credentials:true
 }))
 
@@ -23,13 +26,18 @@ app.use(session({
     // cookie: { secure: true } HTTPS
 }));
 
-app.post('/createAccount', userController.onCreateAccount)
+//app.post('/createAccount',validateCreateUser, userController.onCreateAccount)
 app.post('/api/signIn',userController.onLogin);
 
+app.post('/createAccount', validateCreateUser, userController.onCreateAccount,
+  (req, res) => {
+    res.send(`Hello, ${req.body.name}!`);
+});
 
 app.listen(port, async () => {
     await migrationhelper.migrate()
     await sequelize.authenticate()
     console.log(`Example app listening2 on port ${port}`)
 })
+
 
---
+++ validators/userValidators.js
@@ -0,0 +1,31 @@
+const {check, validationResult} = require('express-validator');
+
+const validateCreateUser = [
+  check('email')
+    .trim()
+    .normalizeEmail() 
+    .isEmail()
+    .withMessage('Invalid email address!')
+    .not()
+    .isEmpty()
+    .withMessage('Empty  email address!'),
+  check('userName')
+    .trim()
+    .not()
+    .isEmpty()
+    .withMessage('Name can not be empty!')
+    .isLength({min: 6})
+    .withMessage('Minimum 6 characters required!'),
+
+  (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(422).json({errors: errors.array()});
+    next();
+  },
+];
+
+module.exports = {
+  //validateLoginUser,
+  validateCreateUser
+}
